1433d7a0d76c5aef593af164c76e795b0244f2e4
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-05-11 10:51:05 +0200

    MainCameraScript
    player script

---
+++ BowlingVRexercise/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{ 
+	public GameObject sphere;
+	public float sphereDistance = 2.5f;
+	public float sphereThrowForce = 200f;
+
+	public bool hold = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (hold)
+		{
+			sphere.transform.position = transform.position + transform.forward * sphereDistance;
+
+			if (GvrViewer.Instance.Triggered || Input.GetKeyDown("space"))
+			{
+				hold = false;
+				sphere.GetComponent<Rigidbody>().useGravity = true;
+				sphere.GetComponent<Rigidbody>().AddForce(transform.forward * sphereThrowForce);
+			}
+		}
+	}
+}
